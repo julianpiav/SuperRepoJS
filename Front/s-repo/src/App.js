d89f683bb37b4cdbe9d2a007e8334b3573431b12
Julian Pinilla <julianpiav@unisabana.edu.co>
2024-11-08 17:14:11 -0500

    Terminacion SuperRepo en local

---
+++ Front/s-repo/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [inspiration, setInspiration] = useState("Haz clic y prepárate para una revelación...");
+
+  const getInspiration = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_BFF_URL}/api/inspire`);
+      if (!response.ok) throw new Error('Error al obtener la inspiración');
+      const data = await response.json();
+      setInspiration(data.message);
+    } catch (error) {
+      console.error("Error fetching inspiration:", error);
+      setInspiration("Hubo un problema al obtener la inspiración. Inténtalo de nuevo.");
+    }
+  };
+  
+
+  const getRandomColor = () => {
+    const colors = ["#FF6347", "#FFD700", "#ADFF2F", "#20B2AA", "#FF69B4"];
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Aplicación de Inspiración</h1>
+      <p>{inspiration}</p>
+      <button
+        onClick={getInspiration}
+        style={{
+          backgroundColor: getRandomColor(),
+          padding: "10px 20px",
+          borderRadius: "5px",
+          border: "none",
+          color: "white",
+          fontSize: "16px",
+          cursor: "pointer",
+        }}
+      >
+        Dame Inspiración
+      </button>
+    </div>
+  );
+}
+
+export default App;
